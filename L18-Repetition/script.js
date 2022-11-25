8d96b7466c0d6450e0f94e7da37cceba2d115f25
Martin Haagen <martin@mbh.se>
2022-11-25 13:59:40 +0100

    Lektion 18

---
+++ L18-Repetition/script.js
@@ -0,0 +1,58 @@
+const apiKey = '9198a31e5a76c8f75d18d60a27544721';
+const loc = 'Rebbetuaröd';
+
+// https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${apiKey}`,
+// https://api.openweathermap.org/data/2.5/weather?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=${apiKey}&units=metric
+/*
+fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${loc}&limit=1&appid=${apiKey}`)
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=${apiKey}&units=metric`)
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })     
+
+console.log('Hejsan Svejsan!');
+*/
+
+/*
+async function getLonLat(location) {
+    let response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${loc}&limit=1&appid=${apiKey}`);
+    let data = await response.json();
+    return { lon: data[0].lon, lat: data[0].lat };     // endast en sak!!
+}
+async function getWeather(lon, lat) {
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+    let data = await response.json();
+    return data;
+}
+async function getData(location) {
+    const geoData = await getLonLat(location);
+    const weather = await getWeather(geoData.lon, geoData.lat);
+    console.log(weather);
+}
+
+getData();
+*/
+
+async function getDataFromUrl(url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
+
+async function getWeather() {
+    const geoData = await getDataFromUrl(`https://api.openweathermap.org/geo/1.0/direct?q=${loc}&limit=1&appid=${apiKey}`);
+    const weather = await getDataFromUrl(`https://api.openweathermap.org/data/2.5/weather?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=${apiKey}&units=metric`)
+    console.log(weather);
+}
+
+getWeather();
